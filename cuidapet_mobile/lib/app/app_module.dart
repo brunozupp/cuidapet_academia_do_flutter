0d8a8636ca29a298a43a7da8e524fcc7a73bc3f2
Bruno Noveli <brunimzuppwolf@gmail.com>
2023-05-15 22:24:08 -0300

    Configurando fluxo para usuário logado

--- cuidapet_mobile/lib/app/app_module.dart
+++ cuidapet_mobile/lib/app/app_module.dart
@@ -1,4 +1,6 @@
 import 'package:cuidapet_mobile/app/modules/auth/auth_module.dart';
+import 'package:cuidapet_mobile/app/modules/core/core_module.dart';
+import 'package:cuidapet_mobile/app/modules/home/home_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppModule extends Module {
@@ -8,6 +10,12 @@ class AppModule extends Module {
 
   @override
   List<ModularRoute> get routes => [
-    ModuleRoute("/auth", module: AuthModule())
+    ModuleRoute("/auth", module: AuthModule()),
+    ModuleRoute("/home", module: HomeModule()),
+  ];
+
+  @override
+  List<Module> get imports => [
+    CoreModule(),
   ];
 }
--- cuidapet_mobile/lib/app/modules/auth/home/auth_home_page.dart
+++ cuidapet_mobile/lib/app/modules/auth/home/auth_home_page.dart
@@ -1,9 +1,47 @@
-import 'package:cuidapet_mobile/app/core/ui/extensions/size_screen_extension.dart';
+import 'package:cuidapet_mobile/app/models/user_model.dart';
 import 'package:flutter/material.dart';
 
-class AuthHomePage extends StatelessWidget {
+import 'package:cuidapet_mobile/app/core/ui/extensions/size_screen_extension.dart';
+import 'package:cuidapet_mobile/app/modules/core/auth/auth_store.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobx/mobx.dart';
 
-  const AuthHomePage({ Key? key }) : super(key: key);
+class AuthHomePage extends StatefulWidget {
+
+  final AuthStore _authStore;
+
+  const AuthHomePage({
+    Key? key,
+    required AuthStore authStore,
+  }) : 
+  _authStore = authStore,
+  super(key: key);
+
+  @override
+  State<AuthHomePage> createState() => _AuthHomePageState();
+}
+
+class _AuthHomePageState extends State<AuthHomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    reaction<UserModel?>((_) => widget._authStore.userLogged, (userLogged) { 
+
+      if(userLogged != null && userLogged.email.isNotEmpty) {
+        // Vai remover todas as outras telas da stack anteriores
+        Modular.to.navigate("/home");
+      } else {
+        Modular.to.navigate("/auth/login");
+      }
+    });
+
+    // Roda ao final do carregamento da página
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) { 
+      widget._authStore.loadUserLogged();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
