e809fe2dc766b2321585f78fa71d2c26fa042572
Bruno Noveli <brunimzuppwolf@gmail.com>
2023-06-04 20:07:37 -0300

    Componente de Mensagem (Snackbar com Asuka)

---
+++ cuidapet_mobile/lib/app/core/ui/widgets/cuidapet_messages.dart
@@ -0,0 +1,14 @@
+import 'package:asuka/asuka.dart';
+
+class CuidapetMessages {
+
+  CuidapetMessages._();
+
+  static void alert(String message) {
+    AsukaSnackbar.alert(message).show();
+  }
+
+  static void info(String message) {
+    AsukaSnackbar.info(message).show();
+  }
+}
