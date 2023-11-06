310607764c75e5e0b455359a0acd1f469cd2d222
Bruno Noveli <brunimzuppwolf@gmail.com>
2023-11-06 17:41:53 -0300

    Implementando layout da lista de Pet Shops

--- cuidapet_mobile/lib/app/modules/home/widgets/home_supplier_tab.dart
+++ cuidapet_mobile/lib/app/modules/home/widgets/home_supplier_tab.dart
@@ -11,17 +11,19 @@ class _HomeSupplierTab extends StatelessWidget {
     return Column(
       children: [
         _HomeTabHeader(controller: controller),
-        Observer(
-          builder: (context) {
-            return AnimatedSwitcher(
-              duration: const Duration(
-                milliseconds: 400,
-              ),
-              child: controller.supplierPageTypeSelected == SupplierPageType.list
-                ? _HomeSupplierList()
-                : _HomeSupplierGrid(),
-            );
-          }
+        Expanded(
+          child: Observer(
+            builder: (context) {
+              return AnimatedSwitcher(
+                duration: const Duration(
+                  milliseconds: 400,
+                ),
+                child: controller.supplierPageTypeSelected == SupplierPageType.list
+                  ? const _HomeSupplierList()
+                  : const _HomeSupplierGrid(),
+              );
+            }
+          ),
         ),
       ],
     );
@@ -79,7 +81,118 @@ class _HomeSupplierList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CustomScrollView(
+      slivers: [
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return const _HomeSupplierListItemWidget();
+            },
+            childCount: 10
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _HomeSupplierListItemWidget extends StatelessWidget {
+  const _HomeSupplierListItemWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 5,
+      ),
+      child: Stack(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(left: 30),
+            width: 1.sw,
+            height: 80.h,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 50),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Clinica central Abc",
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.location_on,
+                              size: 16,
+                            ),
+                            Text("1.34 km de distância")
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  child: CircleAvatar(
+                    backgroundColor: context.primaryColor,
+                    maxRadius: 15,
+                    child: const Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.white,
+                      size: 15,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        
+          Container(
+            margin: const EdgeInsets.only(top: 5),
+            width: 70,
+            height: 70,
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              border: Border.all(
+                color: Colors.transparent,
+                width: 1,
+              ),
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey[100]!,
+                  width: 5,
+                ),
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(100),
+                image: const DecorationImage(
+                  image: NetworkImage(
+                    "https://conteudo.imguol.com.br/c/entretenimento/eb/2022/03/23/cachorro-da-raca-lulu-da-pomeramia-1648065976007_v2_900x506.jpg",
+                  ),
+                  fit: BoxFit.contain,
+                )
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
